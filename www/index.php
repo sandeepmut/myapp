b8ab05a0d6bedd1bc20231814ae9caca5acd6d39
sandeepmut <sandeepmutruwar@gmail.com>
2020-05-07 08:00:10 +0530

    myapp commit

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<!THIS FILE DIVIDES THE BROWSER WINDOW INTO DIFFERENT FRAMESET>
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<HEAD>
+<title>YOGA</TITLE>
+</HEAD>
+<frameset rows="25%,*">
+<FRAME SRC="heading.html"name="f1"scrolling="no">
+<frameset ROWS="100%,*">
+<FRAME SRC="Mainlist.html"name="f2">
+</FRAMESET>
+</FRAMESET>
+</body>
+</HTML>
+
+
+
